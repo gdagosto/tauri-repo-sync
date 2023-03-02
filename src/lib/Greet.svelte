62b85868a831c096e9874dc2fef12037c45700c3
Gabriel d'Agosto <gabriel.dagosto@engenharia.ufjf.br>
2023-03-02 11:06:26 -0300

    initial commit

---
+++ src/lib/Greet.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { invoke } from '@tauri-apps/api/tauri';
+
+	let name = '';
+	let greetMsg = '';
+
+	async function greet() {
+		invoke('run_shell')
+			.then((res) => console.log(res))
+			.catch((e) => console.error(e));
+	}
+</script>
+
+<div>
+	<input id="greet-input" placeholder="Enter a name..." bind:value={name} />
+	<button on:click={greet}>Greet</button>
+	<p>{greetMsg}</p>
+</div>
